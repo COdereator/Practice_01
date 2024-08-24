293cd61cba12b319bd4dd533f418176f68f906e5
Priyanshu Vishwakarma <priyanshuvsk20@gmail.com>
2024-08-24 20:53:46 +0530

    Practice_01

---
+++ src/app/app.component.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { FooterComponent } from "./component/footer/footer.component";
+import { MainComponent } from './component/main/main.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, NavbarComponent, FooterComponent,MainComponent,HeaderComponent,FormsModule,CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Suru karte hai';
+  fname = 'Hello';
+  lname = 'World';
+  binary = 101010;
+  path = '2.jpg';
+  toggle = false
+  greet:any
+  
+  hello(){    
+    if(this.toggle){
+      this.greet=""
+      this.path= '2.jpg'
+      this.toggle= false
+    }
+    else{
+      this.path='1.jpg'
+      this.greet="Hello Boss"
+      this.toggle= true
+    }
+  }
+  // change the value of text with the input
+  changeText:any
+  changes(evt:any){
+    this.changeText = evt.target.value
+  }
+  change = false;
+  color = ""
+  
+  changeColor(){
+    this.change = !this.change
+    if(this.change== true){
+      this.color="red"
+    }
+    else{
+      this.color="green"
+    }
+  }
+}
---
+++ src/app/component/footer/footer.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: '.app-footer',
+  standalone: true,
+  imports: [],
+  // templateUrl: './footer.component.html',
+
+  template:`<p>&copy; Copyright By Hello</p>`,
+  styleUrl: './footer.component.css'
+})
+export class FooterComponent {
+
+}
