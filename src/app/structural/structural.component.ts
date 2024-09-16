dcb9a7977a32231ba24edd687f41483c19f74a70
@priyanshu-vish <priyanshuvsk20@gmail.com>
2024-09-16 11:42:11 +0530

    Update

--- src/app/structural/structural.component.ts
+++ src/app/structural/structural.component.ts
@@ -1,4 +1,4 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
@@ -25,5 +25,14 @@ export class StructuralComponent {
   limitExceed(){
     this.limit++
   }
+
+  users: Array<string> = ["John","Hello","Shawn","Smith","Raj"]
+
+  usersObj: Array<any> = [
+    {id: 1,name: 'John', email: 'john@gmail.com',image:'1.jfif'},
+    {id: 2,name: 'Smith', email: 'smith@gmail.com',image:'2.jfif'},
+    {id: 3,name: 'Sam', email: 'sam@gmail.com',image:'3.jfif'},
+    {id: 4,name: 'Raj', email: 'raj@gmail.com',image:'1.jpg'}
+  ]
 
 }
