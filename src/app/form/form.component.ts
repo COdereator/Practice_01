23050d84954b959d06b072acafe856f316507e5f
Priyanshu Vishwakarma <priyanshuvsk20@gmail.com>
2024-09-28 21:01:36 +0530

    as

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,13 +1,43 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,JsonPipe],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css',
   
 })
 export class FormComponent {
     
+  studentObj:any = {
+    firstName : '',
+    lastName : '',
+    userName : '',
+    city : '',
+    state : '',
+    zipCode : '',
+    isAcceptTerms : false
+  };
+
+  formValue: any;
+  onSubmit() {
+    debugger;
+    this.formValue = this.studentObj;
+  }
+
+  onReset(){
+    this.studentObj = {
+      firstName : '',
+      lastName : '',
+      userName : '',
+      city : '',
+      state : '',
+      zipCode : '',
+      isAcceptTerms : false
+    }
+  }
+
 }
